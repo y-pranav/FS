3866d671acd0bc5a5b9dfea51b671918870b43ec
y-pranav <yyvrpranav@gmail.com>
2025-02-06 17:22:46 +0530

    sliding window 1

---
+++ SlidingWindow/P1.java
@@ -0,0 +1,88 @@
+/*
+You are given an integer array nums and two integers l and r. Your task is to 
+find the minimum positive energy produced by a sequence of operations. 
+Each operation corresponds to selecting a contiguous subarray of nums 
+whose length is between l and r (inclusive).
+
+The energy of a sequence is defined as the product of all the numbers in 
+the subarray. You need to find the sequence with the smallest positive energy.
+
+If no such sequence exists, return -1.
+
+Input Format:
+---------------
+Line-1: Three space separated integers, N, L and R.
+Line-2: N space separated integers, array[].
+
+Output Format:
+-----------------
+An integer result, smallest positive energy.
+
+Sample Input-1:
+-----------------
+4 2 3
+2 -1 3 4
+
+Sample Output-1:
+-------------------
+12
+
+Explanation:
+--------------
+The possible sequences of operations with lengths between l = 2 and r = 3 are:
+
+[2, -1] (not valid, energy = -2)
+[3, 4] (energy = 12)
+[2, -1, 3] (not valid, energy = -6)
+The sequence [3, 4] produces the smallest positive energy of 12. Hence, 
+the output is 12.
+
+Sample Input-2:
+-----------------
+3 2 3
+-1 -3 2
+
+Sample Output-1:
+-------------------
+3
+
+Constraints:
+============
+1 ≤ nums.length ≤ 100
+1 ≤ l ≤ r ≤ nums.length
+−1000 ≤ nums[i] ≤ 1000
+ */
+
+import java.util.*;
+
+class P1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int L = sc.nextInt();
+        int R = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+    
+        long res = Long.MAX_VALUE;
+        
+        for (int start = 0; start < n; start++) {
+            long prod = 1;
+            for (int end = start; end < n && end - start + 1 <= R; end++) {
+                if (nums[end] == 0) {
+                    prod = 0;
+                    break;
+                }
+                prod *= nums[end];
+                
+                int len = end - start + 1;
+                if (len >= L && prod > 0) {
+                    res = Math.min(res, prod);
+                }
+            }
+        }
+        System.out.println(res == Long.MAX_VALUE ? -1 : res);
+    }
+}
---
+++ SlidingWindow/P3.java
@@ -0,0 +1,88 @@
+/*
+You are an architect tasked with designing a series of connected rooms in a 
+building. You are given a list of room sizes represented by an integer array 
+roomSizes, an integer maxFrequency representing the maximum number of times 
+a particular room size can appear, and an integer maxArea representing 
+the maximum allowable total area of connected rooms. A set of connected 
+rooms is called viable if the frequency of each room size in this set is 
+less than or equal to maxFrequency, and the total area of the rooms in 
+this set is less than or equal to maxArea. 
+
+Return the length of the longest viable set of connected rooms from roomSizes.
+
+Input Format:
+-------------
+Line-1: 3 space separated integers, n, maxFrequency, maxArea
+Line-2: N space separated integers, roomSizes[].
+
+Output Format:
+-------------
+An integer, the length of the longest viable set of connected rooms
+
+
+Sample Input-1:
+---------------
+8 2 10
+1 2 3 1 2 3 1 2
+
+Sample Output-1:
+----------------
+5 
+
+Explanation: 
+------------
+The longest possible viable set of connected rooms is [1, 2, 3, 1, 2] since 
+the room sizes 1, 2, and 3 appear at most twice, and the total area is less than 10.
+
+Sample Input-2:
+---------------
+6 1 3
+1 2 1 2 1 2
+
+Sample Output-2:
+----------------
+2
+
+Explanation: The longest possible viable set of connected rooms is [1, 2] since 
+the room sizes 1 and 2 appear at most once, and the total area is 3.
+
+Constraints:
+------------
+1 <= roomSizes.length <= 10^5
+1 <= roomSizes[i] <= 10^4 where roomSizes[i] is the size of the i-th room.
+1 <= maxFrequency <= roomSizes.length
+1 <= maxArea <= 10^9
+ 
+*/
+
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int mf = sc.nextInt();
+        int ma = sc.nextInt();
+        sc.nextLine();
+        String[] input = sc.nextLine().split(" ");
+        
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(input[i]);
+        }
+        
+        int temp = 0, left = 0, res = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int right = 0; right < n; right++) {
+            temp += a[right];
+            mp.put(a[right], mp.getOrDefault(a[right], 0) + 1);
+            while ((mp.get(a[right]) > mf || temp > ma) && left <= right) {
+                mp.put(a[left], mp.get(a[left]) - 1);
+                temp -= a[left];
+                left++;
+            }
+            res = Math.max(right - left + 1, res);
+        }
+        System.out.println(res);
+    }
+}
