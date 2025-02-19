c62181959df4734da0f419eb67eabdfeab786597
y-pranav <yyvrpranav@gmail.com>
2025-02-19 21:27:26 +0530

    day 7

---
+++ Trees/P10.java
@@ -0,0 +1,39 @@
+// Mr. Rakesh is interested in working with Data Structures.
+
+// He has constructed a Binary Tree (BT) and asked his friend 
+// Anil to check whether the BT is a self-mirror tree or not.
+
+// Can you help Rakesh determine whether the given BT is a self-mirror tree?
+// Return true if it is a self-mirror tree; otherwise, return false.
+
+// Note:
+// ------
+// In the tree, '-1' indicates an empty (null) node.
+
+// Input Format:
+// -------------
+// A single line of space separated integers, values at the treenode
+
+// Output Format:
+// --------------
+// Print a boolean value.
+
+
+// Sample Input-1:
+// ---------------
+// 2 1 1 2 3 3 2
+
+// Sample Output-1:
+// ----------------
+// true
+
+
+// Sample Input-2:
+// ---------------
+// 2 1 1 -1 3 -1 3
+
+// Sample Output-2:
+// ----------------
+// false
+
+
---
+++ Trees/P9.java
@@ -0,0 +1,55 @@
+/*
+Bubloo is working with computer networks, where servers are connected 
+in a hierarchical structure, represented as a Binary Tree. Each server (node) 
+is uniquely identified by an integer value.
+
+Bubloo has been assigned an important task: find the shortest communication 
+path (in terms of network hops) between two specific servers in the network.
+
+Network Structure:
+------------------
+The network of servers follows a binary tree topology.
+Each server (node) has a unique identifier (integer).
+If a server does not exist at a certain position, it is represented as '-1' (NULL).
+
+Given the root of the network tree, and two specific server IDs (E1 & E2), 
+determine the minimum number of network hops (edges) required to 
+communicate between these two servers.
+
+Input Format:
+-------------
+Space separated integers, elements of the tree.
+
+Output Format:
+--------------
+Print an integer value.
+
+
+Sample Input-1:
+---------------
+1 2 4 3 5 6 7 8 9 10 11 12
+4 8
+
+Sample Output-1:
+----------------
+4
+
+Explanation:
+------------
+The edegs between 4 and 8 are: [4,1], [1,2], [2,3], [3,8]
+
+
+Sample Input-2:
+---------------
+1 2 4 3 5 6 7 8 9 10 11 12
+6 6
+
+Sample Output-2:
+----------------
+0
+
+Explanation:
+------------
+No edegs between 6 and 6.
+ 
+*/    
